cdfe4f3ef397936d4f7dea8a4d5717a891cd6015
Matheus Pavoski <mmaschio@student.42sp.org.br>
2023-07-26 08:07:12 -0300

    ft_strlen 42

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/25 15:22:36 by mpavoski          #+#    #+#             */
-/*   Updated: 2023/07/25 15:46:14 by mpavoski         ###   ########.fr       */
+/*   Created: 2023/07/26 08:01:46 by mmaschio          #+#    #+#             */
+/*   Updated: 2023/07/26 08:01:54 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned long int	ft_strlen (char *str)
+unsigned long int	ft_strlen(char *str)
 {
 	int	c;
 

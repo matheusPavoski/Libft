d2b4830498d9aa78760aad333f75bd0d1ddf6697
matheusPavoski <mpavoski@DESKTOP-NROEBAS.localdomain>
2023-07-27 15:42:22 -0300

    ft_memcpy alterar header 42

---
+++ ft_memcpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/27 15:10:54 by mpavoski          #+#    #+#             */
+/*   Updated: 2023/07/27 15:40:17 by mpavoski         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*ft_memcpy(void *dest, const void *src, size_t num_b)
+{
+	unsigned char			*dst;
+	const unsigned char 	*sorc;
+	size_t					i;
+
+	dst = (unsigned char *)dest;
+	sorc = (const unsigned char *)src;
+	i = 0;
+	while(i < num_b)
+	{
+		dst[i] = sorc[i];
+		i++;
+	}
+	return(dst);
+}
+
+/*#include <string.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	src[] = "Super Mario 64";
+	char	dest[20];
+	ft_memcpy(dest, src, 5);
+	printf("%s\n", dest);
+	return(0);
+}*/

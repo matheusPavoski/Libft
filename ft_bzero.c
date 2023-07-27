20d2a3d32c494a60db5675453ae301ca05b10a1c
matheusPavoski <mpavoski@DESKTOP-NROEBAS.localdomain>
2023-07-27 10:17:03 -0300

    ft_bzero corrigida

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 09:23:38 by mpavoski          #+#    #+#             */
-/*   Updated: 2023/07/27 10:02:40 by mpavoski         ###   ########.fr       */
+/*   Updated: 2023/07/27 10:16:00 by mpavoski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,10 @@ void	ft_bzero(void *ptr_m, size_t num_b)
 #include <string.h>
 int	main(void)
 {
-	char str[] = "The Legeend of Zelda";
+	char str[] = "The Legend of Zelda";
 
 	printf("Antes: %s\n", str);
-	ft_bzero(str, 6);
+	ft_bzero(str, 19);
 	printf("Depois: %s\n", str);
 	return (0);
 }*/

317796e56bee35413a3a01ec65b9b6b292d92888
matheusPavoski <mpavoski@DESKTOP-NROEBAS.localdomain>
2023-07-27 15:43:07 -0300

    alterar header 42

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 09:06:53 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/07/27 09:59:02 by mpavoski         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:39:56 by mpavoski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,5 @@ int					ft_isprint(int c);
 unsigned long int	ft_strlen(char *str);
 void				*ft_memset(void *ptr_mem, int val_to_exc, size_t num_b);
 void 				ft_bzero(void *ptr_m, size_t num_b);
+void				*ft_memcpy(void *dest, const void *src, size_t num_b);
 #endif

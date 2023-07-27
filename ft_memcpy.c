c107c976da77680febccd866bbf1a4dd60745ffa
matheusPavoski <mpavoski@DESKTOP-NROEBAS.localdomain>
2023-07-27 15:47:28 -0300

    Olhar header 42

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,12 @@
 /*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 15:10:54 by mpavoski          #+#    #+#             */
-/*   Updated: 2023/07/27 15:40:17 by mpavoski         ###   ########.fr       */
+/*   Updated: 2023/07/27 15:46:28 by mpavoski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void	*ft_memcpy(void *dest, const void *src, size_t num_b)
 {
 	unsigned char			*dst;

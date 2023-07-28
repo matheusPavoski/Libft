e033e7ebe4d056875f9342ab356807cfd4940bc5
matheusPavoski <theusopavoski@gmail.com>
2023-07-28 16:20:19 -0300

    ft_strchr wkg

---
+++ ft_strchr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 15:46:32 by mpavoski          #+#    #+#             */
+/*   Updated: 2023/07/28 16:18:15 by mpavoski         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if(str[i] == c)
+			return ((char *)&str[i]);
+		i++;		
+	}
+	return ((char *)0);
+}
+
+/*#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	const char str[] = "Oi, Mundo!";
+	
+	printf("%s\n", ft_strchr(str, ' '));
+	printf("%s\n", strchr(str, ' '));
+	return (0);
+}*/

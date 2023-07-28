f6ebbaa8432309a1693b47471234e91c150b5091
matheusPavoski <theusopavoski@gmail.com>
2023-07-28 14:51:42 -0300

    updt + re: 42

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 09:06:53 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/07/28 12:32:47 by mpavoski         ###   ########.fr       */
+/*   Updated: 2023/07/28 14:46:07 by mpavoski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@ int					ft_isalpha(int c);
 int					ft_isascii(int c);
 int					ft_isdigit(int c);
 int					ft_isprint(int c);
-unsigned long int	ft_strlen(char *str);
+size_t				ft_strlen(const char *str);
 void				*ft_memset(void *ptr_mem, int val_to_exc, size_t num_b);
 void 				ft_bzero(void *ptr_m, size_t num_b);
 void				*ft_memcpy(void *dest, const void *src, size_t num_b);
 void				*ft_memmove(void *dest, const void *src, size_t num_b);
 size_t				ft_strlcpy(char *dest, const char *src, size_t len);
+size_t				ft_strlcat(char *dest, const char *src, size_t num_b);
 #endif

d6bb996018135e74384ea8e6be347292dcef1aa7
matheusPavoski <theusopavoski@gmail.com>
2023-08-01 18:19:34 -0300

    norm check

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: mpavoski <mpavoski@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 15:39:08 by mpavoski          #+#    #+#             */
-/*   Updated: 2023/08/01 08:13:08 by mpavoski         ###   ########.fr       */
+/*   Updated: 2023/08/01 18:15:21 by mpavoski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,37 @@
 
 void	*ft_memchr(const void *ptr, int val_b, size_t num_b)
 {
+	const unsigned char		*str;
+	unsigned char			val;
+	size_t					i;
 
+	str = (const unsigned char *)ptr;
+	val = val_b;
+	i = 0;
+	while (i < num_b)
+	{
+		if (str[i] == val)
+			return ((char *)&str[i]);
+		i++;
+	}
+	return (NULL);
 }
+
+/*#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	const unsigned char	str[] = "The Legend Of Zelda";
+	unsigned char		val = 'Z';
+	size_t				num_b = 15;
+
+	if ((ft_memchr(str, val, num_b) != NULL) && (memchr(str, val, num_b) != NULL))
+	{
+		printf("%s\n", (char *)ft_memchr(str, val, num_b));
+		printf("%s\n", (char *)memchr(str, val, num_b));
+	}
+	else
+		printf("%s\n", "Caractere não encontrado.");
+	return (0);
+}*/

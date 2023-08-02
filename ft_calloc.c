7f9e63618fd6136bb8ba1c3ac2f8c796ad2b97a7
Matheus Pavoski <mmaschio@student.42sp.org.br>
2023-08-02 16:11:39 -0300

    wkg - re:Tests

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 15:45:46 by mmaschio          #+#    #+#             */
+/*   Updated: 2023/08/02 16:10:39 by mmaschio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, (nmemb * size));
+	return (ptr);
+}

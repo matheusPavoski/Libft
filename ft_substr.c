f7db4d4b4351be139d4429388b6640c13e0bf394
matheusPavoski <theusopavoski@gmail.com>
2023-08-15 19:21:58 -0300

    frcheck

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 13:32:23 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/15 14:32:45 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/15 19:20:44 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,27 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	new_strlen;
-	size_t	s_len;
-	char	*new_str;
+	size_t	substr_len;
+	size_t	str_len;
+	char	*substr;
 
-	new_str = (char *)malloc(sizeof(*s) * (len + 1));
-	if (new_str == 0)
+	if (!s)
 		return (NULL);
-	new_strlen = 0;
-	s_len = 0;
-	while (s[new_strlen] != '\0')
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		len = 0;
+	else if (start + len > str_len)
+		len = str_len - start;
+	substr = (char *)malloc(sizeof(*substr) * (len + 1));
+	if (!substr)
+		return (NULL);
+	substr_len = 0;
+	while (substr_len < len && s[start] != '\0')
 	{
-		if ((new_strlen >= start) && (s_len < len))
-		{
-			new_str[s_len] = s[new_strlen];
-			s_len++;
-		}
-		new_strlen++;
+		substr[substr_len] = s[start];
+		substr_len++;
+		start++;
 	}
-	new_str[s_len] = 0;
-	return (new_str);
+	substr[substr_len] = '\0';
+	return (substr);
 }

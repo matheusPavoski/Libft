8acc3fb540cfbf39db21bbd04956202b3d9f682e
matheusPavoski <theusopavoski@gmail.com>
2023-08-17 11:29:23 -0300

    frcheck

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 11:00:57 by mmaschio          #+#    #+#             */
+/*   Updated: 2023/08/17 11:19:00 by mmaschio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = (char *) malloc(ft_strlen(s) + 1);
+	if (!s || !f)
+		return (NULL);
+	if (!str)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str [i] = f(i, (char)s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}

8e988e4e2383c2db5de56448d643725f80473532
matheusPavoski <theusopavoski@gmail.com>
2023-08-17 14:11:54 -0300

    Updt

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:10:16 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/17 12:14:57 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/17 13:44:47 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,4 +49,5 @@ char				**ft_split(char const *s, char c);
 char				*ft_itoa(int n);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				ft_striteri(char *s, void (*f)(unsigned int, char*));
+void				ft_putchar_fd(char c, int fd);
 #endif

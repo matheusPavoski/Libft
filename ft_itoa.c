db5b939739361058d77bbf2eef15738ae56cd62f
matheusPavoski <theusopavoski@gmail.com>
2023-08-17 10:58:02 -0300

    frcheck

---
+++ ft_itoa.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 10:40:34 by mmaschio          #+#    #+#             */
+/*   Updated: 2023/08/17 10:55:03 by mmaschio         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	cnt_digits(int nd)
+{
+	int	len;
+
+	len = 0;
+	if (nd <= 0)
+		len++;
+	while (nd != 0)
+	{
+		nd = nd / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		i;
+	char	*str;
+
+	i = cnt_digits(n);
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	if (n < 0)
+		str[0] = '-';
+	while (n != 0)
+	{
+		if (str[0] == '-')
+			str[i--] = '0' - (n % 10);
+		else
+			str[i--] = '0' + (n % 10);
+		n = n / 10;
+	}
+	return (str);
+}

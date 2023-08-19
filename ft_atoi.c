94dccf52616b09868e0819986b53b996f9d0dad0
matheusPavoski <theusopavoski@gmail.com>
2023-08-19 16:10:55 -0300

    updt + frcheck

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:05:29 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/15 20:25:58 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/19 16:09:13 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,13 @@ int	ft_atoi(const char *str)
 	i = 0;
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
 		i++;
-	if (str[i] == '-' || str[i] == '+')
-		sign = 1 - 2 * (str[i++] == '-');
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+			i++;
 	while (str[i] >= '0' && str[i] <= '9')
 		result = 10 * result + (str[i++] - '0');
 	return (result * sign);

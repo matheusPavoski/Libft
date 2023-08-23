33e3c32313fbef67da6a97af3f1804c9c914c476
Matheus Pavoski <mmaschio@student.42sp.org.br>
2023-08-23 19:36:05 -0300

    Updt + frcheck + 42

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:08:04 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/23 19:26:39 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/23 19:34:44 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memset(void *ptr_mem, int val_to_exc, size_t num_b)
 	unsigned char	val;
 	size_t			i;
 
-	ptr = ptr_mem;
+	ptr = (unsigned char *)ptr_mem;
 	val = (unsigned char)val_to_exc;
 	i = 0;
 	while (i < num_b)

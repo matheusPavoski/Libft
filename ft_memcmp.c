81f0edb3904479ebac8571ce697eab2552ddc51a
matheusPavoski <theusopavoski@gmail.com>
2023-08-31 19:20:11 -0300

    Updt proto + note

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,22 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:07:34 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/15 20:27:47 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/31 18:31:24 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const void *ptr1, const void *ptr2, size_t num_b)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	const unsigned char	*str1;
 	const unsigned char	*str2;
 	size_t				i;
 
-	str1 = (const unsigned char *) ptr1;
-	str2 = (const unsigned char *) ptr2;
+	str1 = (const unsigned char *) s1;
+	str2 = (const unsigned char *) s2;
 	i = 0;
-	while (i < num_b)
+	while (i < n)
 	{
 		if (str1[i] != str2[i])
 		{

ee7b23271bc817e8659f8390ec987cc8d2fa76af
matheusPavoski <theusopavoski@gmail.com>
2023-08-31 20:31:21 -0300

    Updt + note

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:10:16 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/31 19:26:38 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/31 19:28:16 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ size_t				ft_strlcat(char *dst, const char *src, size_t size);
 int					ft_toupper(int c);
 int					ft_tolower(int c);
 char				*ft_strchr(const char *s, int c);
-char				*ft_strchr(const char *s, int c);
+char				*ft_strrchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);

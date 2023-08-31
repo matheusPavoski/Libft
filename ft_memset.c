61ec97c074b94370a7e846556189df81ad2abb9b
matheusPavoski <theusopavoski@gmail.com>
2023-08-31 19:20:56 -0300

    Updt proto + note

--- ft_memset.c
+++ ft_memset.c
@@ -6,22 +6,22 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:08:04 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/31 03:06:25 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/31 18:34:17 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *ptr_mem, int val_to_exc, size_t num_b)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*ptr;
 	unsigned char	val;
 
-	ptr = (unsigned char *)ptr_mem;
-	val = (unsigned char)val_to_exc;
-	while (num_b--)
+	ptr = (unsigned char *)s;
+	val = (unsigned char)c;
+	while (n--)
 	{
-		ptr[num_b] = val;
+		ptr[n] = val;
 	}
 	return (ptr);
 }

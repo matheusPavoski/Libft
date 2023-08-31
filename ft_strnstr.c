5a9630156e90250a9c52a38cabdbcfc412a2d47d
matheusPavoski <theusopavoski@gmail.com>
2023-08-31 19:22:52 -0300

    Updt proto + note

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,24 +6,24 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:09:06 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/15 20:29:27 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/31 19:03:58 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *str, const char *substr, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	len_substr;
+	size_t	len_little;
 
-	len_substr = ft_strlen(substr);
-	if (*substr == '\0')
-		return ((char *)str);
-	while (*str && len_substr <= len--)
+	len_little = ft_strlen(little);
+	if (*little == '\0')
+		return ((char *)big);
+	while (*big && len_little <= len--)
 	{
-		if (ft_strncmp(str, substr, len_substr) == 0)
-			return ((char *)str);
-		str++;
+		if (ft_strncmp(big, little, len_little) == 0)
+			return ((char *)big);
+		big++;
 	}
 	return (NULL);
 }

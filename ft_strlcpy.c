c2da62098e45bbd79cf1a3bad4214e1e21cdda8e
matheusPavoski <theusopavoski@gmail.com>
2023-08-31 19:22:11 -0300

    Updt proto + note

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,24 +6,24 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:08:42 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/15 20:29:05 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/08/31 18:59:45 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t len)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	if (len == 0)
+	if (size == 0)
 		return (ft_strlen(src));
-	while ((i < len - 1) && (src[i] != '\0'))
+	while ((i < size - 1) && (src[i] != '\0'))
 	{
-		dest[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	dst[i] = '\0';
 	return (ft_strlen(src));
 }

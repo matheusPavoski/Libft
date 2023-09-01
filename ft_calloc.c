d186c794131652f1fd0857e4ab716baacc5d3181
Matheus Pavoski <mmaschio@student.42sp.org.br>
2023-09-01 01:54:47 -0300

    updt + 42

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mmaschio <mmaschio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 17:06:15 by mmaschio          #+#    #+#             */
-/*   Updated: 2023/08/31 18:25:15 by mmaschio         ###   ########.fr       */
+/*   Updated: 2023/09/01 01:30:34 by mmaschio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	if (!nmemb || !size)
+		return (malloc(0));
+	if (nmemb > __SIZE_MAX__ / size)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (ptr);
